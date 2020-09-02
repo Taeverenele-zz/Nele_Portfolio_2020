b23d4d0f99a3c23541535e100b6c03205248b570
Nele Taevere <ntaevere@gmail.com>
2020-09-02 15:14:52 +1000

    adjust title and paragraph's background color when hamburger button is clicked

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function showNavBar() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  let nav = document.getElementById("myTopnav");
+  let title = document.querySelector('.title');
+  let paragraph = document.querySelector('.paragraph');
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
+    title.className += " background";
+    paragraph.className += " background";
   } else {
-    x.className = "topnav";
+    nav.className = "topnav";
+    title.className = "title";
+    paragraph.className = "paragraph";
   }
 }
-
